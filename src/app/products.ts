807d19d2bf5f289d5e3588a6e8e35b12a8e1bb9c
bogdan g <bogdan g@>
2022-10-14 12:24:58 +0300

    update products data

--- src/app/products.ts
+++ src/app/products.ts
@@ -5,13 +5,17 @@ export interface Product{
     description: string;
 }
 
-export const products=[
+export const products: Product[] = [
     {
         id: 1,
-        name: 'product 1'
+        name: 'product 1',
+        price: 100,
+        description: 'desc1'
     },
     {
         id: 2,
-        name: 'product 2'
+        name: 'product 2',
+        price: 200,
+        description: 'desc2'
     }
 ];

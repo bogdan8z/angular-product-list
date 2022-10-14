6642ade52be63de8c08d914df84dc78af08e85d2
bogdan g <bogdan g@>
2022-10-14 12:31:24 +0300

    update product list; update product list; remove unused file

--- src/app/products.ts
+++ src/app/products.ts
@@ -2,20 +2,25 @@ export interface Product{
     id: number;
     name: string;
     price: number;
-    description: string;
+    description?: string;
 }
 
 export const products: Product[] = [
     {
         id: 1,
-        name: 'product 1',
-        price: 100,
-        description: 'desc1'
+        name: 'Phone XL',
+        price: 799,
+        description: 'A large phone with one of the best screens'
     },
     {
         id: 2,
-        name: 'product 2',
-        price: 200,
-        description: 'desc2'
+        name: 'Phone Mini',
+        price: 699,
+        description: 'A great phone with one of the best cameras'        
+    },
+    {
+        id: 3,
+        name: 'Phone Standard',
+        price: 299
     }
 ];

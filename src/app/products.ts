acd76783829935a81e7a0f3f05da87315cba876d
bogdan g <bogdan g@>
2022-10-14 12:21:10 +0300

    add products data

---
+++ src/app/products.ts
@@ -0,0 +1,17 @@
+export interface Product{
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+}
+
+export const products=[
+    {
+        id: 1,
+        name: 'product 1'
+    },
+    {
+        id: 2,
+        name: 'product 2'
+    }
+];
